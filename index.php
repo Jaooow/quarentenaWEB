85f7f75ba16e366dd120baa468cbbebd28ca7294
Jaooow <joaopedro_cc2017@hotmail.com>
2020-06-10 18:30:45 -0300

    Jogo da Memoria

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
 	<body>
 		<h2><u>Exercicios Quarentena</u></h2>
 		
-		<a href = "2020.19.05/soma.php">Lista de Exercicios - SOMA</a>
-		
+		<a href = "2020.19.05/soma.php">Lista de Exercicios - SOMA</a> <br/>
+		<a href = "2020.03.06/jogo_memoria.php">Jogo da Memória</a>
 	</body>
 	<foot>
 		<p>
-			By: &copy João Pedro Catarina Conçolaro
+			By: &copy João Pedro Catarina Conçolaro.
 		</p>
 	</foot>
 </html>
