d25a39b1f28c89ab9e77ddef838d3c3f009c484a
GLeoni17 <gabriel.lduarte@outlook.com>
2020-06-30 17:35:52 -0300

    Nova versao 30 06 2020

--- 2020.06.17/dama.php
+++ 2020.06.17/dama.php
@@ -38,25 +38,27 @@
 			}
 		</style>
 		<script>
+
+			var jogada='b';
+			var id_jogada;
+
 			function verificaposicao(nopai, cor){
+				if(jogada==cor){
+					id_jogada = nopai.id;
 				
-				
-				
-				td = document.getElementsByClassName('preto');
-				
-				for(i=0;i<td.length;i++){
-					td[i].style.borderColor = 'black';
-					td[i].style.borderWidth = '1px';
-					td[i].style.backgroundColor = 'lightgray';
-				}
+				apaga();
 				
 				linha = nopai.id[3];
 				coluna = nopai.id[4];
 				
 				if(cor == 'b'){
 					diagonal_esquerda = ((parseInt(linha)+1) +''+ (parseInt(coluna)-1));
-					diagonal_direita = ((parseInt(linha)+1) +''+ (parseInt(coluna)+1)); 
-					
+					diagonal_direita = ((parseInt(linha)+1) +''+ (parseInt(coluna)+1));
+
+				}else{
+					diagonal_esquerda = ((parseInt(linha)-1) +''+ (parseInt(coluna)-1));
+					diagonal_direita = ((parseInt(linha)-1) +''+ (parseInt(coluna)+1));
+				}	
 				
 					if(document.getElementById("dot"+diagonal_esquerda) != null){
 						if(document.getElementById("dot"+diagonal_esquerda).innerHTML == ''){
@@ -68,15 +70,63 @@
 						if(document.getElementById("dot"+diagonal_direita).innerHTML == ''){
 							document.getElementById("dot"+diagonal_direita).style.borderColor = 'green';
 							document.getElementById("dot"+diagonal_direita).style.backgroundColor = 'lightgreen';
+						}else{
+
+							peca=document.getElementById("dot"+diagonal_direita).innerHTML[17];
+							
+							if((cor=='b' && peca=='p') || (cor=='p' && peca=='b')){
+								
+								if(cor == 'b'){
+									diagonal_direita_comer = ((parseInt(linha)+2) +''+ (parseInt(coluna)+2));
+								}else{
+									diagonal_direita_comer = ((parseInt(linha)-2) +''+ (parseInt(coluna)+2));
+								}
+
+								console.log(diagonal_direita_comer);
+
+								if(document.getElementById("dot"+diagonal_direita_comer) != null){
+									if(document.getElementById("dot"+diagonal_direita_comer).innerHTML == ''){
+										document.getElementById("dot"+diagonal_direita_comer).style.borderColor = 'green';
+										document.getElementById("dot"+diagonal_direita_comer).style.backgroundColor = 'lightgreen';
+									}
+								}
+
+							}
 						}
 					}
 					
+
+
+				}else{
+					alert("Não é sua vez");
 				}
-				else{
-					
-				}
+				
 					
 			}
+			
+			function verificajogada(celula){
+				if(celula.style.backgroundColor=='lightgreen'){
+					celula.innerHTML=document.getElementById(id_jogada).innerHTML;
+					document.getElementById(id_jogada).innerHTML='';
+					apaga();
+					if(jogada=='b'){
+						jogada='p';
+					}else{
+						jogada='b';
+					}
+				}
+			}
+
+			function apaga(){
+				td = document.getElementsByClassName('preto');
+				
+				for(i=0;i<td.length;i++){
+					td[i].style.borderColor = 'black';
+					td[i].style.borderWidth = '1px';
+					td[i].style.backgroundColor = 'lightgray';
+				}
+			}
+					
 		</script>
 	</head>
 	<body>
@@ -91,7 +141,7 @@
 								echo '<td class="branco" id = "dot'.$i.$j.'">';
 							}
 							else{
-								echo '<td class="preto" id = "dot'.$i.$j.'">';
+								echo '<td class="preto" onclick="verificajogada(this)" id = "dot'.$i.$j.'">';
 								if(($i) < 3)
 								{
 									echo'<span class="dot_branco" onclick="verificaposicao(this.parentNode, \'b\')"></span>';
